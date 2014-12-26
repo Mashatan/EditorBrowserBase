02b82e8bcf84789afcbe919c32e3d32a9cd5545f
Mashatan <ali.mashatan@gmail.com>
2014-12-26 19:47:01 +0330

    Clean-up

--- src/main.cpp
+++ src/main.cpp
@@ -17,26 +17,11 @@ int main(int argc, char ** argv)
     QApplication app(argc, argv);
     QQuickView view;
 
-#if 0
-    Sample sample;
-    view.engine()->rootContext()->setContextProperty("cppClass",&sample);
-#endif
-
     qmlRegisterType<ETCodeEditorExtension>("ETCodeEditorExtension", 1, 0, "ETCodeEditorExtension");
 
 
     view.setSource(QUrl("qrc:///qml/main.qml"));
     view.show();
 
-#if 0
-    QObject *object = (QObject*)view.rootObject();
-
-
-    QVariant returnedValue;
-    QVariant msg = "Hello from C++";
-    QMetaObject::invokeMethod(object, "myQmlFunction",
-            Q_RETURN_ARG(QVariant, returnedValue),
-            Q_ARG(QVariant, msg));
-#endif
     return app.exec();
 }
